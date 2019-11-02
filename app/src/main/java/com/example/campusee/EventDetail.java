e5786c23a15f756379d69ba59df7c7f4aab9f36e
Rebecca430 <yihanlin@usc.edu>
2019-11-01 19:14:12 -0700

    asd

--- app/src/main/java/com/example/campusee/EventDetail.java
+++ app/src/main/java/com/example/campusee/EventDetail.java
@@ -14,17 +14,17 @@ public class EventDetail extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event_detail);
-        // TODO: create the intent from user_dashboard and pass event name
-        Intent intent = getIntent();
-        mEvent = intent.getSerializableExtra("event");
-        ((TextView)findViewById(R.id.event_title)).setText(mEvent.Title);
-        ((TextView)findViewById(R.id.event_description)).setText(mEvent.Description);
-        String date = String.format("%02d.%02d.%4d", mEvent.month, mEvent.date, mEvent.year);
-        ((TextView)findViewById(R.id.event_date)).setText(date);
-        String time = String.format("%02d : %02d", mEvent.hour, mEvent.minute);
-        ((TextView)findViewById(R.id.event_time)).setText(time);
-        String email = "Contact " + mEvent.AuthorEmail + " if you have any questions";
-        ((TextView)findViewById(R.id.event_email)).setText(email);
-        ((ImageView)findViewById(R.id.event_image)).setImageURI(mEvent.DownloadUrls[]);
+//        // TODO: create the intent from user_dashboard and pass event name
+//        Intent intent = getIntent();
+//        mEvent = intent.getSerializableExtra("event");
+//        ((TextView)findViewById(R.id.event_title)).setText(mEvent.Title);
+//        ((TextView)findViewById(R.id.event_description)).setText(mEvent.Description);
+//        String date = String.format("%02d.%02d.%4d", mEvent.month, mEvent.date, mEvent.year);
+//        ((TextView)findViewById(R.id.event_date)).setText(date);
+//        String time = String.format("%02d : %02d", mEvent.hour, mEvent.minute);
+//        ((TextView)findViewById(R.id.event_time)).setText(time);
+//        String email = "Contact " + mEvent.AuthorEmail + " if you have any questions";
+//        ((TextView)findViewById(R.id.event_email)).setText(email);
+//        ((ImageView)findViewById(R.id.event_image)).setImageURI(mEvent.DownloadUrls[]);
     }
 }
