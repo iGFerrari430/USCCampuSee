8d13630d4078b2b4635a90d9294ff49639600c0e
Rebecca <yihanlin@usc.edu>
2019-11-01 19:04:47 -0700

    qizhurangwopull

--- app/src/main/java/com/example/campusee/EventDetail.java
+++ app/src/main/java/com/example/campusee/EventDetail.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class EventDetail extends AppCompatActivity {
@@ -14,8 +15,16 @@ public class EventDetail extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event_detail);
         // TODO: create the intent from user_dashboard and pass event name
-//        Intent intent = getIntent();
-//        mEvent = intent.getStringExtra("event");
-//        ((TextView)findViewById(R.id.event_title)).setText(mName);
+        Intent intent = getIntent();
+        mEvent = intent.getSerializableExtra("event");
+        ((TextView)findViewById(R.id.event_title)).setText(mEvent.Title);
+        ((TextView)findViewById(R.id.event_description)).setText(mEvent.Description);
+        String date = String.format("%02d.%02d.%4d", mEvent.month, mEvent.date, mEvent.year);
+        ((TextView)findViewById(R.id.event_date)).setText(date);
+        String time = String.format("%02d : %02d", mEvent.hour, mEvent.minute);
+        ((TextView)findViewById(R.id.event_time)).setText(time);
+        String email = "Contact " + mEvent.AuthorEmail + " if you have any questions";
+        ((TextView)findViewById(R.id.event_email)).setText(email);
+        ((ImageView)findViewById(R.id.event_image)).setImageURI(mEvent.DownloadUrls[]);
     }
 }
